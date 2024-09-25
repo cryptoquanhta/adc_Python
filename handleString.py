31776cbefa278c1d153ad3f2f4678a6345032ada
khaint <nguyentaikhai123@gmail.com>
2024-09-25 22:56:44 +0700

    handle string 1

---
+++ handleString.py
@@ -0,0 +1,45 @@
+string = 'Kevin Nguyen'
+#1 Capitallize()
+
+#2 Center(): tra ve chuoi o giua
+
+#3 count()
+print(string.count('i'))
+
+#4 encode()
+
+#5 decode()
+
+#6 endswith(): co ket thuc vs ky tu ko
+
+#7 expandtabs(): thay the \t bang 1 khoang trang len = 8
+
+#8 find(): tra ve vi tri bat dau
+
+#9 index(): giong find(), neu ko thay tra ve exception
+
+#10 isalnum(): tra ve true neu chi chua number and string
+
+#11 isalpha(): tra ve true neu chi chua string
+
+#12 isdigit(): = true neu chứa duy nhất các chữ số
+
+#13 islower(): = true neu in thuong
+
+#14 isupper(): = true neu in hoa
+
+#15 isnumeric(): = true neu chỉ chứa duy nhất các ký tự số
+
+#16 isspace(): = true neu chi chua khoang trang
+
+#17 istitle(): = true neu string la title(cac chu cai dau tien viet hoa)
+
+#18 join()
+string_one = ' '
+string_two = 'TAI'
+print(string_one.join(string_two))
+# Kết quả: T A I
+string_one1 = '-'
+string_two1 = ['T','D','C',]
+print(string_one1.join(string_two1))
+# Kết quả: T-D-C
